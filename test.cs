3f1067586fbfee266c8888f0dcf27eace9782814
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-02 15:10:46 +0300

    02.05.2023 Buutti Buutcamp

--- test.cs
+++ test.cs
@@ -1,5 +1,9 @@
 ﻿//testing with Python and C#, C# doesn't run the file at all, unlike Python and JS
 
+//prints Hello World!, but not the if statement
+console.WriteLine("Hello World!");
+
+//tested if statement
 if (console.WriteLine("wt"))
 {
     console.WriteLine(true);
