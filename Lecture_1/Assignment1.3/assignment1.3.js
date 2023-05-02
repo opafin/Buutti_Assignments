aaf3e7144ecaeb035dca06280fd3d72aa769fcea
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-02 11:33:23 +0300

    2.5.2023 Buutti Buutcamp

---
+++ Lecture_1/Assignment1.3/assignment1.3.js
@@ -0,0 +1,13 @@
+const numToOperate = 100;
+const operatingNumber = 200;
+
+let combination = numToOperate + operatingNumber;
+
+console.log(combination);
+
+console.log("exponent", numToOperate ** operatingNumber);
+console.log("modulo", numToOperate % operatingNumber);
+console.log("division", numToOperate / operatingNumber);
+console.log("multiplication", numToOperate * operatingNumber);
+console.log("minus", numToOperate - operatingNumber);
+console.log("average", (numToOperate + operatingNumber) / 2);
