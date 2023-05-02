902fc677a678de899e22e59211cc5c2ae26b0a73
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-02 10:39:59 +0300

    assignments 2.5.2023

---
+++ Lecture_1/Assignment1.2/assignment1.2.js
@@ -0,0 +1,25 @@
+// let can be changed afterward
+// constants main var can't be reassigned, but deeper variables can be changed
+
+//letit, stringit ja muuttuvat muuttujat
+
+//Javascript, ints, floats, doubles, are all numbers
+
+const example = "monkeys";
+console.log(typeof example);
+
+const text = 12 + " monkeys"; // 12 monkeys
+const text2 = 6 + 6 + " monkeys"; // 12 monkeys
+const text3 = "monkeys" + 6 + 6 + 6; // monkeys 666
+
+const tarzansFriends = "monkees";
+const tarzanLikesJane = true;
+const monkeysLike = 69;
+
+console.log(tarzansFriends, typeof tarzansFriends);
+console.log(tarzanLikesJane, typeof tarzanLikesJane);
+console.log(monkeysLike, typeof monkeysLike);
+
+tarzanLikesJane = false; // can't reassign tarzans love
+
+//prettier extension
