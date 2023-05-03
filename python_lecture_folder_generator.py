a46671e22ced58b6a04660a7bbe995779707c96f
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-03 14:06:11 +0300

    03.05.2023 Buutti Buutcamp

--- python_lecture_folder_generator.py
+++ python_lecture_folder_generator.py
@@ -1,18 +1,33 @@
 import os
 
-lecture_number = input("which lecture is this? (enter a number) ")
-how_many_to_generate = int(input("how many assignments to generate? (enter a number) "))
-print(f"generating {how_many_to_generate} assignment-folders and files for Lecture {lecture_number}" )
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clear_console()
 
+print("**Welcome to the Lecture Folder Generator!**")
+lecture_number = input("which lecture is this? (enter a number) ")
+clear_console()
+
+how_many_to_generate = int(input("set the total count of assignments: (enter a number) "))
+clear_console()
+
+print(f"GENERATING {how_many_to_generate} assignment-folders and -files for Lecture {lecture_number}\nDONE" )
+print("The total count of assignments can be increased later")
+print("e.g from 3 to 15. Just run the script again.\n")
+print("previous assignments will not be overwritten, duplicates will not be created.\n")
 
 lecture_folder = f"Lecture_{lecture_number}"
 if not os.path.exists(lecture_folder):
     os.makedirs(lecture_folder)
 
 for i in range(0, how_many_to_generate):
+    
+    #setting the format to 00, 01, 02, etc.
+    format_i = str(i).zfill(2)
+
     #setting directory and file names in the loop
-    assignment_folder = f"Assignment{lecture_number}.{i}"
-    assignment_file = f"assignment{lecture_number}.{i}.js"
+    assignment_folder = f"Assignment{lecture_number}.{format_i}"
+    assignment_file = f"assignment{lecture_number}.{format_i}.js"
 
     # creating the directories
     full_assignment_folder_path = os.path.join(lecture_folder, assignment_folder)
