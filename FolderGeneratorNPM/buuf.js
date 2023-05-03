a63856de739bd8f161d2b11c172314de4217a91c
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-03 22:00:37 +0300

    03.05.2023 Buutti Buutcamp

---
+++ FolderGeneratorNPM/buuf.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+const lectureNumber = process.argv[2];
+const howManyToGenerate = process.argv[3];
+
+const lectureFolder = `Lecture_${lectureNumber}`;
+
+// Get the current working directory
+const projectRoot = process.cwd();
+
+// creating the lecture folder
+const fullLectureFolderPath = path.join(projectRoot, lectureFolder);
+if (!fs.existsSync(fullLectureFolderPath)) {
+  fs.mkdirSync(fullLectureFolderPath);
+}
+
+for (let i = 0; i <= howManyToGenerate; i++) {
+  const formattedI = String(i).padStart(2, "0");
+
+  const assignmentFolder = `Assignment${lectureNumber}.${formattedI}`;
+  const assignmentFile = `assignment${lectureNumber}.${formattedI}.js`;
+
+  const fullAssignmentFolderPath = path.join(
+    fullLectureFolderPath,
+    assignmentFolder
+  );
+
+  // Creating the assignment folder inside the lecture folder
+  if (!fs.existsSync(fullAssignmentFolderPath)) {
+    fs.mkdirSync(fullAssignmentFolderPath);
+  }
+
+  // Creating the assignment file inside the assignment folder
+  const fullAssignmentFilePath = path.join(
+    fullAssignmentFolderPath,
+    assignmentFile
+  );
+  if (!fs.existsSync(fullAssignmentFilePath)) {
+    fs.writeFileSync(fullAssignmentFilePath, "");
+  }
+}
