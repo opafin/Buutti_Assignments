9540bda0b9e3c4f7b709220f6540141f75851c6f
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-04 16:58:59 +0300

    04.05.2023 Buutti Buutcamp

---
+++ Lecture_2/Assignment2.03/assignment2.03.js
@@ -0,0 +1,9 @@
+console.log("Fancy feature");
+
+//git branch fancyFeature
+//git checkout fancyFeature
+//git add assignment2.02.js
+//git commit -m "Fancy feature"
+//git push origin fancyFeature
+//git checkout main
+//then merge request, review and merging done in gitlab
