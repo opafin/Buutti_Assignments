6c9de9b37bb8b0e930f889cb26de1e922ac7ac39
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-05 16:37:19 +0300

    05.05.2023 Buutti Buutcamp

--- Lecture_2/Assignment2.08/assignment2.08.js
+++ Lecture_2/Assignment2.08/assignment2.08.js
@@ -1,1 +1,6 @@
 //using Gitlens to view changes in gitrepo
+// Commit Graph + merging branches + local and origin
+// blame/fileblame
+// heatmap/file changes
+// navigating between different changes
+// also looked into timeline and other related features
