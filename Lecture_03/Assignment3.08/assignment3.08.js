8310275d1cc5dd43deb9c7e13238e709a261c0fe
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-08 16:00:29 +0300

    08.05.2023 Buutti Buutcamp

--- Lecture_03/Assignment3.08/assignment3.08.js
+++ Lecture_03/Assignment3.08/assignment3.08.js
@@ -1,40 +1,37 @@
 function foloopfun(n) {
-    var sum = 0;
-    for (var i = 0; i <= n; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            sum += i;
-        }
+  var sum = 0;
+  for (var i = 0; i <= n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
     }
-    return sum;
+  }
+  return sum;
 }
 function whilefun(n) {
-    var i = 0;
-    var sum = 0;
-    while (i <= n) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            sum += i;
-            i++;
-        }
+  var i = 0;
+  var sum = 0;
+  while (i <= n) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+      i++;
     }
-    return sum;
+  }
+  return sum;
 }
 function fizzBuzz(n) {
-    var i = 0;
-    while (i <= n) {
-        i++;
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        }
-        else if (i % 3 === 0) {
-            console.log("Fizz");
-        }
-        else if (i % 5 === 0) {
-            console.log("Buzz");
-        }
-        else {
-            console.log(i);
-        }
+  var i = 0;
+  while (i <= n) {
+    i++;
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
     }
+  }
 }
 console.log(foloopfun(17));
 console.log(whilefun(17));
