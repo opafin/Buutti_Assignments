5075ded2e1abd229e3797411b3ca16d942e99015
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-08 10:25:24 +0300

    08.05.2023 Buutti Buutcamp

---
+++ Lecture_03/Assignment3.01/index.js
@@ -0,0 +1,7 @@
+// const pi = require("mathjs").pi; work stoo
+// const round = require("mathjs").round; works too
+
+//to get import to work, you need to add "type": "module" to package.json
+import { pi, round } from "mathjs";
+
+console.log(round(pi, 10));
