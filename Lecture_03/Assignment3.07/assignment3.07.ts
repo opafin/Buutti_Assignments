c04b44225b432c1b554549e819c83c1c58e1b97d
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-08 15:11:51 +0300

    08.05.2023 Buutti Buutcamp

---
+++ Lecture_03/Assignment3.07/assignment3.07.ts
@@ -0,0 +1,28 @@
+
+function fun(n: number): number{
+
+    let sum: number = 0
+    for (let i = 1; i < n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+function whileSolution(n: number): number{
+    
+    let i = 1
+    let sum = 0
+
+    while (i < n)
+    {
+    sum += i
+    i++
+    }
+    
+    return sum
+
+    }
+
+console.log(fun(10));
+console.log(whileSolution(10));
+
