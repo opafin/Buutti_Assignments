126c16c08717096fc4cacb1040c812e53984d5d5
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-08 15:58:52 +0300

    08.05.2023 Buutti Buutcamp

---
+++ Lecture_03/Assignment3.08/assignment3.08.js
@@ -0,0 +1,42 @@
+function foloopfun(n) {
+    var sum = 0;
+    for (var i = 0; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+function whilefun(n) {
+    var i = 0;
+    var sum = 0;
+    while (i <= n) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+            i++;
+        }
+    }
+    return sum;
+}
+function fizzBuzz(n) {
+    var i = 0;
+    while (i <= n) {
+        i++;
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+console.log(foloopfun(17));
+console.log(whilefun(17));
+console.log(fizzBuzz(20));
+fizzBuzz(20);
--- Lecture_03/Assignment3.08/assignment3.08.ts
+++ Lecture_03/Assignment3.08/assignment3.08.ts
@@ -3,8 +3,8 @@
 function foloopfun(n: number): number{
 
     let sum: number = 0
-    for (let i = 1; i < n; i++) {
-        if (i % 3 === 0 && i % 3 === 0){
+    for (let i = 0; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0){
             sum += i;
         }
     }
@@ -13,19 +13,64 @@ function foloopfun(n: number): number{
 
 function whilefun(n: number): number{
     
-    let i = 1
+    let i = 0
     let sum = 0
 
-    while (i < n)
+    while (i <= n)
     {
-        if (i % 3 === 0 && i % 3 === 0) 
+        if (i % 3 === 0 || i % 5 === 0)
+        { 
         sum += i
         i++
+        }
     }
     
     return sum
 
     }
+    
+function fizzBuzz(n: number): void{
+    
+    let i = 0
 
-console.log(foloopfun(10));
-console.log(whilefun(10));
+    while (i <= 20)
+    {
+        i++;
+
+        if (i % 3 === 0 && i % 5 === 0)
+        { 
+        console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0){
+        console.log("Fizz");
+        }
+        else if (i % 5 === 0){
+        console.log("Buzz");
+        }
+        else
+        {
+        console.log(i);
+        }        
+    }
+}
+
+
+
+function pyramid(n: number): void{
+    
+    let i = 0
+    let tree = ""
+
+    while (i <= n)
+    {
+
+        console.log(tree += "&");
+        i++;
+    }
+
+    }
+
+// fizzBuzz(20);
+// console.log(foloopfun(17));
+// console.log(whilefun(17));
+pyramid(5);
