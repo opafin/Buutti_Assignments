68fca244cd1c939e3d2ce42e3c29a28c3b7f35f9
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-10 12:42:26 +0300

    10.05.2023 Buutti Buutcamp

--- Lecture_04/Assignment4.01/assignment4.01.ts
+++ Lecture_04/Assignment4.01/assignment4.01.ts
@@ -1,10 +1,8 @@
 function capitalizer(stringi: string): void {
-  let words = stringi.split(' ')
+  const words = stringi.split(' ')
   words.forEach((word, index) => {
     words[index] = word[0].toLocaleUpperCase() + word.slice(1)
   })
   console.log(...words)
 }
 capitalizer('this is a very nice string')
-
-var kalle = 'stringi'
--- Lecture_04/Assignment4.02/assignment4.02.ts
+++ Lecture_04/Assignment4.02/assignment4.02.ts
@@ -1,7 +1,7 @@
 function generateARandom(min: number, max: number): number {
-  const multiplier = Math.random();
-  const range = max + min - 1;
-  const randomInteger = Math.floor(range * multiplier) + 1;
-  return randomInteger;
+  const multiplier = Math.random()
+  const range = max + min - 1
+  const randomInteger = Math.floor(range * multiplier) + 1
+  return randomInteger
 }
-console.log(generateARandom(1, 100));
+console.log(generateARandom(1, 100))
