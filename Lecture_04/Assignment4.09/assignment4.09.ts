ca6d00a120c48113f60d28a87fa4074a67c25fc7
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-10 19:01:22 +0300

    10.05.2023 Buutti Buutcamp

--- Lecture_04/Assignment4.09/assignment4.09.ts
+++ Lecture_04/Assignment4.09/assignment4.09.ts
@@ -1,9 +1,10 @@
 const numbers = [749385, 498654, 234534, 345467, 956876, 365457, 235667, 464534, 346436, 873453]
 
-const newArray = numbers.filter((number: number) => {
+const newArray = numbers.filter((number) => {
   if (number % 3 === 0 || number % 5 === 0) {
-    if (number % 5 === 0 && number % 3 === 0) {
+    if (number % 5 !== 0 || number % 3 !== 0) {
       return number
     }
   }
 })
+console.log(newArray)
--- Lecture_04/Assignment4.10/assignment4.10.ts
+++ Lecture_04/Assignment4.10/assignment4.10.ts
@@ -20,3 +20,8 @@ namerific.find((name) => {
 function checkForPat(name: string) {
   if (name[name.length - 1] === 't' && name.length === 3) return name
 }
+
+// can do without declaring the function separately, too
+namerific.find((name) => {
+  if (name[name.length - 1] === 't' && name.length === 3) return name
+})
