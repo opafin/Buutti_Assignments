816006a1511003b51b8a688b2124aad2744ddcb8
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-14 21:39:13 +0300

    14.05.2023 Buutti Buutcamp

--- Lecture_04/Assignment4.17/assignment4.17.ts
+++ Lecture_04/Assignment4.17/assignment4.17.ts
@@ -1,9 +1,11 @@
+// #SOLUTION 1
 // high level function smash solution
 console.log(process.argv[2].split(' ').map((a) => a.split('').reverse().join('')).join(' '))
 
 const long = 'Tarzan thought this was a very long sentence. Then he saw Jane reverse the words in place using JavaScript and his mind was blown'
 const gnol = 'nazraT thguoht siht saw a yrev gnol .ecnetnes nehT eh was enaJ esrever eht sdrow ni ecalp gnisu tpircSavaJ dna sih dnim saw nwolb'
 
+// #SOLUTION 2
 // og foloop heard reduce() has an accumulator, so og foloop got two, and did the job walking backwards
 let mainAccumulator = '' 
 let childAccumulator = ''
@@ -17,6 +19,7 @@ for (let k=long.length; k >= 0; k--) {
 }
 console.log(mainAccumulator.trim());
 
+// #SOLUTION 3
 // the "for of loop" can directly iterate string characters 
 // without a split(), ...spread or specifying a string[index]
 let mainAcc = ''
@@ -31,7 +34,8 @@ for(const char of long)
 }
 console.log(mainAcc.trim());
 
-// // this works too: the fabulous reduce and foloop together
+// #SOLUTION 4
+// this works too: the fabulous reduce and foloop together
 console.log(long.split(' ').reduce((a, b, i, d) => {
     for (let j = b.length -1; j >= 0;j--) {
      a += b[j]   
@@ -40,21 +44,22 @@ console.log(long.split(' ').reduce((a, b, i, d) => {
     else return a + " "
 }, ''))
  
-
-// // spread, and reduce inside reduce -inception for maximal fabulousity.
+// #SOLUTION 5
+// spread, and reduce inside reduce -inception for maximal fabulousity.
 console.log(long.split(' ').reduce((a, b, i, d) => {
     a += [...b].reduce((x, y) => y.concat(x),'') 
     if(i ===  d.length -1) return a 
     else return a + " "
 }, ""))
 
-// // instead of using .split(''), [...spread] is used to turn the string into an array
-// // unlike the og foloop, reduce() is confused by strings and needs an array
+// #JEEJEE about #SOLUTION 5 & reduce()
+// instead of using .split(''), [...spread] is used to turn the string into an array
+// unlike the og foloop, reduce() is confused by strings and needs an array
 
-// // [...b].reduce((x, y) => y.concat(x),'') this is the part that reverses the string
-// // in the concat() call the reduce's() accumulator and the element switch places, reversing the string.
+// [...b].reduce((x, y) => y.concat(x),'') this is the part that reverses the string
+// in the concat() call the reduce's() accumulator and the element switch places, reversing the string.
 
-// // reduce()'s provide direct access to array details and an accumulator
-// // another benefit is that because the params used with reduce()&callbkfn are always the same, it's
-// // sometimes easier to read than an og foloop surrounded by custom variables.
+// reduce()'s provide direct access to array details and an accumulator
+// another benefit is that because the params used with reduce()&callbkfn are always the same, it's
+// sometimes easier to read than an og foloop surrounded by custom variables.
 
