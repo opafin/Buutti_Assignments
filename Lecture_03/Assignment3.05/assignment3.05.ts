cabf8fdc5abf36f23353875241d4da3fdf295e7f
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-14 21:18:49 +0300

    12.05.2023 Buutti Buutcamp

--- Lecture_03/Assignment3.05/assignment3.05.ts
+++ Lecture_03/Assignment3.05/assignment3.05.ts
@@ -1,20 +1,22 @@
-const arr = ["banaani", "omena", "mandariini", "appelsiini", "kurkku", "tomaatti", "peruna"];
+const arr = ['banaani', 'omena', 'mandariini', 'appelsiini', 'kurkku', 'tomaatti', 'peruna']
 
-console.log(arr[2], arr[3], arr.length);
-const sortedArray = arr.sort((a, b) => a.localeCompare(b));
-console.log(sortedArray);
-arr.push("sipuli");
-console.log(arr);
-arr.shift();
-console.log(arr);
+console.log(arr[2], arr[3], arr.length)
+arr.sort()
+console.log(arr)
+arr.push('sipuli')
+console.log(arr)
+arr.shift()
+console.log(arr)
 
 arr.forEach((element) => {
-  console.log(element);
-});
+  console.log(element)
+})
 
-console.log();
+console.log()
 arr.forEach((element) => {
-  if (element.includes("r")) {
-    console.log(element);
+  if (element.includes('r')) {
+    console.log(element)
   }
-});
+})
+
+// fixed!
