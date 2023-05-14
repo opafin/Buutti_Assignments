e9d8870a4c0f5fbced624ebd1594a39b0f63623e
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-14 21:29:10 +0300

    12.05.2023 Buutti Buutcamp

--- Lecture_03/Assignment3.09/assignment3.09.ts
+++ Lecture_03/Assignment3.09/assignment3.09.ts
@@ -1,19 +1,19 @@
 const fizzBuzz = (n: number) => {
-  let i = 1;
+  let i = 0
 
   while (i < n) {
-    i++;
+    i++
 
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
+      console.log('FizzBuzz')
     } else if (i % 3 === 0) {
-      console.log("Fizz");
+      console.log('Fizz')
     } else if (i % 5 === 0) {
-      console.log("Buzz");
+      console.log('Buzz')
     } else {
-      console.log(i);
+      console.log(i)
     }
   }
-};
+}
 
-fizzBuzz(100);
+fizzBuzz(100)
