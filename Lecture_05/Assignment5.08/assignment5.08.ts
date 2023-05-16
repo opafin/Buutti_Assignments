e8d4b49b4d59d8d2cb0b33797ae34e59045074ee
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-16 14:21:57 +0300

    16.05.2023 Buutti Buutcamp

---
+++ Lecture_05/Assignment5.08/assignment5.08.ts
@@ -0,0 +1,12 @@
+// Assignment 5.8: Forecast
+// Create forecast_data.json and then add to following data to
+
+// {
+//   "day": "monday",
+//   "temperature": 25,
+//   "cloudy": true,
+//   "sunny": false,
+//   "windy": false
+// }
+
+// Then retrieve the data and modify the temperature of that forecast. Lastly, save the change back to the file.
