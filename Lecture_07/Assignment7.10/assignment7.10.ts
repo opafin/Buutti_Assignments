8c205d00b7733c809aab60014f2476a786028b46
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-05-28 23:18:58 +0300

    26.05.2023 Buutti Buutcamp

--- Lecture_07/Assignment7.10/assignment7.10.ts
+++
@@ -1,7 +0,0 @@
-// Assignment 7.10: Cats and Dogs
-// You can get random dog images from Dog CEO API.
-// You can get random cat images from Cat as a Service.
-// You can get random fox images from Random Fox
-// Create and deployn an Azure function that returns randomly either a cat picture or a dog picture. But for every 13th request it returns a fox picture!
-// The return format should be a JSON object with a message and a link to the picture.
-// Extra: Make your function return HTML so that when used, the client sees the picture, and the message as a heading.
