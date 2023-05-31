425c97926e3dabbc51e635baf4fbc0ae1573854e
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-06-01 00:35:14 +0300

    1.6.2023 Buutti Buutcamp

--- Lecture_09/Assignment9.08/src/middlewares.ts
+++ Lecture_09/Assignment9.08/src/middlewares.ts
@@ -7,7 +7,6 @@ export const middleware = (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body)
   }
   const time = new Date()
-  console.log(time.getHours().toString().padStart(2, '0'), time.getMinutes().toString().padStart(2, '0'))
   console.log(req.method)
   console.log(req.path)
   console.log(time)
