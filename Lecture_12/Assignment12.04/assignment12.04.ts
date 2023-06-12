437595bfc33caeeeb102f4fce773e362fcd9e896
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-06-12 17:14:18 +0300

    12.6.2023 Buutti Buutcamp

---
+++ Lecture_12/Assignment12.04/assignment12.04.ts
@@ -0,0 +1,17 @@
+// 1. query the library
+// "2000-01-01 00:00:00"
+// "2000-02-12 00:00:00"
+// "2000-02-12 00:00:00"
+
+// //2. "2000-01-15 00:00:00"
+// "2000-03-05 00:00:00"
+// "2000-03-08 00:00:00"
+
+//3.
+// "Teppo Testaaja"	"2000-03-03 00:00:00"
+// "Teppo Testaaja"	"2000-01-01 00:00:00"
+// "Teppo Testaaja"	"2000-03-03 00:00:00"
+
+//4. "Silmarillion"	1977	"English"
+// "The Hitchhikers Guide to the Galaxy"	1979	"English"
+// "The Walking Dead: Days Gone Bye"	2008	"English"
---
+++ Lecture_12/Assignment12.06/src/db.ts
@@ -0,0 +1,41 @@
+// db.ts
+import pg from 'pg'
+
+const { PG_HOST, PG_PORT, PG_USERNAME, PG_PASSWORD, PG_DATABASE, NODE_ENV } = process.env
+
+const pool = new pg.Pool({
+  host: PG_HOST,
+  port: Number(PG_PORT),
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  database: PG_DATABASE,
+  ssl: NODE_ENV === 'development'
+})
+
+const executeQuery = async (query: string, parameters?: Array<any>) => {
+  const client = await pool.connect()
+  try {
+    const result = await client.query(query, parameters)
+    return result
+  } catch (error: any) {
+    console.error(error.stack)
+    error.name = 'dbError'
+    throw error
+  } finally {
+    client.release()
+  }
+}
+
+export const createProductsTable = async () => {
+  const query = `
+        CREATE TABLE IF NOT EXISTS "products" (
+            "id" SERIAL PRIMARY KEY,
+            "name" VARCHAR(100) NOT NULL,
+            "price" REAL NOT NULL
+        )`
+  await executeQuery(query)
+  console.log('Products table initialized')
+}
+
+// serial autogeneroi id:n
+// primary key = yksilöllinen
